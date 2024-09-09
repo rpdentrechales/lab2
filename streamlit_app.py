33ae5808a80152eaf1bd19b711ed7eed15431a99
rpdentrechales <rpdprocorpo@gmail.com>
2024-09-09 22:05:52 +0000

    Updating files

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,8 @@ import streamlit as st
 numero_1 = st.text_input("escreve um número 1")
 st.write("+")
 numero_2 = st.text_input("escreve um número 2")
-st.write(f"{numero_1} + {numero_2} = {int(numero_1) + int(numero_1)}")
+
+if numero_1 and numero_2:
+  st.write(f"{numero_1} + {numero_2} = {numero_1 + numero_1}")
+
+
