e465f014ec06c06bb0e269a915615f579d823937
rpdentrechales <rpdprocorpo@gmail.com>
2024-09-24 18:36:29 +0000

    Updating files

--- views/conversor.py
+++ views/conversor.py
@@ -18,7 +18,14 @@ col_1,col_2 = st.columns(2)
 with col_1:
   seletor_moeda_1 = st.selectbox("Selecione uma moeda de",siglas_moedas,index=0)
   seletor_moeda_2 = st.selectbox("Selecione uma moeda para",siglas_moedas,index=20)
+
+  conversao_moeda_1 = dados["rates"][seletor_moeda_1]
+  conversao_moeda_2 = dados["rates"][seletor_moeda_2]
+
   valor_para_conversao = st.number_input("Selecione o valor para converter",format="%0.2f")
 
 with col_2:
   st.markdown("## R$ 100")
+
+  st.write(conversao_moeda_1)
+  st.write(conversao_moeda_2)
