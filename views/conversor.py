f543458d62c6764144da28d86106fa3315e7213e
rpdentrechales <rpdprocorpo@gmail.com>
2024-09-24 18:21:31 +0000

    Updating files

--- views/conversor.py
+++ views/conversor.py
@@ -1,4 +1,14 @@
 
 import streamlit as st
+import requests
+import json
+
+url = "https://api.exchangerate-api.com/v4/latest/USD"
+my_resquest = requests.get(url)
+
+content = my_resquest.content
+dados = json.loads(content)
 
 st.title("Conversor de moeda")
+st.write(dados)
+
