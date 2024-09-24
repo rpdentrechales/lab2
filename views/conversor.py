13ce154c2428a326d721fbfb727cab94d04c4948
rpdentrechales <rpdprocorpo@gmail.com>
2024-09-24 18:26:27 +0000

    Updating files

--- views/conversor.py
+++ views/conversor.py
@@ -14,7 +14,7 @@ st.title("Conversor de moeda")
 col_1,col_2 = st.columns(2)
 
 with col_1:
-  seletor_moeda_1 = st.selectbox("Selecione uma moeda de",["moeda_1","moeda_2"])
+  seletor_moeda_1 = st.selectbox("Selecione uma moeda de",["moeda_1","moeda_2"],placeholder="moeda_2")
   seletor_moeda_2 = st.selectbox("Selecione uma moeda para",["moeda_1","moeda_2"])
   valor_para_conversao = st.number_input("Selecione o valor para converter",min_value=0)
 
