be7c21e67b06a3f75aaf95c2a596aea02094ef72
rpdentrechales <rpdprocorpo@gmail.com>
2024-09-24 18:32:36 +0000

    Updating files

--- views/conversor.py
+++ views/conversor.py
@@ -9,13 +9,15 @@ my_resquest = requests.get(url)
 content = my_resquest.content
 dados = json.loads(content)
 
+siglas_moedas = list(dados["rates"].keys())
+
 st.title("Conversor de moeda")
 
 col_1,col_2 = st.columns(2)
 
 with col_1:
-  seletor_moeda_1 = st.selectbox("Selecione uma moeda de",["moeda_1","moeda_2"])
-  seletor_moeda_2 = st.selectbox("Selecione uma moeda para",["moeda_1","moeda_2"])
+  seletor_moeda_1 = st.selectbox("Selecione uma moeda de",siglas_moedas)
+  seletor_moeda_2 = st.selectbox("Selecione uma moeda para",siglas_moedas)
   valor_para_conversao = st.number_input("Selecione o valor para converter",format="%0.2f")
 
 with col_2:
