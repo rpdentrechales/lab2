84ff478627451a5c21c15828e221eb1d95a2b550
rpdentrechales <rpdprocorpo@gmail.com>
2024-10-08 18:59:28 +0000

    Updating files

--- views/forca.py
+++ views/forca.py
@@ -5,7 +5,7 @@ import random
 lista_palavras = []
 
 # Abrir o arquivo de palavras
-with open('/dados/palavras.txt', 'r') as palavras_file:
+with open('dados/palavras.txt', 'r') as palavras_file:
     for line in palavras_file:
         lista_palavras.append(line.strip())
 
