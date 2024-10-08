7001fe804fd15339a9e9cf7ca182710e67666127
rpdentrechales <rpdprocorpo@gmail.com>
2024-10-08 18:59:01 +0000

    Updating files

--- views/forca.py
+++ views/forca.py
@@ -2,4 +2,11 @@
 import streamlit as st
 import random
 
+lista_palavras = []
+
+# Abrir o arquivo de palavras
+with open('/dados/palavras.txt', 'r') as palavras_file:
+    for line in palavras_file:
+        lista_palavras.append(line.strip())
+
 st.title("Jogo da Forca")
