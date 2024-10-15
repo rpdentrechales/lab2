70154e3b1bd8723f4e3a02a7afc93dfcb61c4f66
rpdentrechales <rpdprocorpo@gmail.com>
2024-10-15 18:58:33 +0000

    Updating files

--- views/forca.py
+++ views/forca.py
@@ -15,10 +15,31 @@ st.title("Jogo da Forca")
 if "palavra_secreta" not in st.session_state:
   palavra_secreta = random.choice(lista_palavras) # Selecionar uma palavra aleatória
   st.session_state["palavra_secreta"] = palavra_secreta
+  palavra_chutada = [] # Criar uma lista com os chutes
+
+  for letra in palavra_secreta:
+    # Iniciar a palavra de chutes com um monte de traço
+    palavra_chutada.append("_")
+  
+  st.session_state["palavra_chutada"] = palavra_chutada
 
 st.write(st.session_state["palavra_secreta"])
 
 chute = st.text_input("Chute uma letra",max_chars=1)
 
+acertos = 0
+tentativas = 5
+acertou = False
+
 if st.button("chutar"):
-  st.write(chute)
+  palavra_secreta = st.session_state["palavra_secreta"]
+  palavra_chutada = st.session_state["palavra_chutada"]
+
+  for index, letra in enumerate(palavra_secreta):
+    # Verificando se a letra está na palavra
+    if chute == letra:
+      palavra_chutada[index] = letra
+
+  palavra_chutada_print = " ".join(palavra_chutada)
+
+  st.write(f"{palavra_chutada_print}")
