e728151bd0f56338d54cbb31a7e1ebe8bf61f5c4
rpdentrechales <rpdprocorpo@gmail.com>
2024-12-06 19:58:12 +0000

    Updating files

--- views/exemplo_thales.py
+++ views/exemplo_thales.py
@@ -6,8 +6,8 @@ import pandas as pd
 # Title for the app
 st.title("Exemplo Thales")
 
-dados_exemplo = {"Coluna_1": [1,2,3,4,5,6,7,8],
-                "Coluna_2": [1,2,3,4,5,6,7,8],
+dados_exemplo = {"Coluna_1": [1,2,3,2,2,2,2,2],
+                "Coluna_2": [1,2,3,2,5,2,8],
                 "Coluna_3": [1,2,3,4,5,6,7,8],
                 "Coluna_4": [1,2,3,4,5,6,7,8]}
 
