bd07eb3c86dfaf5b2f42eae917d8ec662eca6a56
rpdentrechales <rpdprocorpo@gmail.com>
2024-12-06 20:00:00 +0000

    Updating files

--- views/exemplo_thales.py
+++ views/exemplo_thales.py
@@ -22,4 +22,4 @@ groupby_df = pd.DataFrame(df.groupby(colunas_groupby).sum())
 colunas_2 = groupby_df.columns.unique()
 colunas_groupby = st.multiselect("Selecione a coluna dataframe",colunas)
 
-st.dataframe(groupby_df[colunas_2])
+st.dataframe(groupby_df[[colunas_2]])
